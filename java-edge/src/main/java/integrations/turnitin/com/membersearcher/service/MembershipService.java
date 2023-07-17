8172239a9dc729269de8196353688b43874817ad
rigade <105679116+rigade@users.noreply.github.com>
2023-07-16 22:32:05 -0700

    update the method to fetch all users at once

--- java-edge/src/main/java/integrations/turnitin/com/membersearcher/service/MembershipService.java
+++ java-edge/src/main/java/integrations/turnitin/com/membersearcher/service/MembershipService.java
@@ -1,9 +1,16 @@
 package integrations.turnitin.com.membersearcher.service;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.Map;
+import java.util.List;
 
 import integrations.turnitin.com.membersearcher.client.MembershipBackendClient;
 import integrations.turnitin.com.membersearcher.model.MembershipList;
+import integrations.turnitin.com.membersearcher.model.Membership;
+import integrations.turnitin.com.membersearcher.model.User;
+import integrations.turnitin.com.membersearcher.model.UserList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,9 +27,8 @@ public class MembershipService {
 	 * associates them with their corresponding membership.
 	 *
 	 * @return A CompletableFuture containing a fully populated MembershipList object.
-	 */
-	public CompletableFuture<MembershipList> fetchAllMembershipsWithUsers() {
-		return membershipBackendClient.fetchMemberships()
+
+	 return membershipBackendClient.fetchMemberships()
 				.thenCompose(members -> {
 					CompletableFuture<?>[] userCalls = members.getMemberships().stream()
 							.map(member -> membershipBackendClient.fetchUser(member.getUserId())
@@ -31,5 +37,51 @@ public class MembershipService {
 					return CompletableFuture.allOf(userCalls)
 							.thenApply(nil -> members);
 				});
+
+	 */
+	public CompletableFuture<MembershipList> fetchAllMembershipsWithUsers() {
+//		CompletableFuture<UserList> usersList = membershipBackendClient.fetchUsers();
+//
+//		return membershipBackendClient.fetchMemberships()
+//				.thenCombine(usersList, (memberships, users) -> {
+//					Map<String, User> userMap = users.getUsers().stream()
+//							.collect(Collectors.toMap(User::getId, user -> user));
+//
+//					for (Membership membership : memberships.getMemberships()) {
+//						User user = userMap.get(membership.getUserId());
+//						membership.setUser(user);
+//					}
+//
+//					return memberships;
+//				});
+		CompletableFuture<UserList> usersList = membershipBackendClient.fetchUsers();
+//		if(userList == null )
+//		{
+//			throw new NullPointerException("UserList is null");
+//		}
+		return membershipBackendClient.fetchMemberships()
+				.thenCombine(usersList, (memberships, users) -> {
+					if (users == null) {
+						// Handle null users appropriately
+						throw new IllegalStateException("Users list is null.");
+					}
+
+					Map<String, User> userMap = users.getUsers().stream()
+							.collect(Collectors.toMap(User::getId, user -> user));
+
+					for (Membership membership : memberships.getMemberships()) {
+						String userId = membership.getUserId();
+						if (userId != null) {
+							User user = userMap.get(userId);
+							membership.setUser(user);
+						} else {
+							// Handle null user ID appropriately
+							throw new IllegalStateException("Membership user ID is null.");
+						}
+					}
+
+					return memberships;
+				});
+
 	}
 }
